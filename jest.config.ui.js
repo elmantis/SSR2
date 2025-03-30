fad39f0529f1ef5a1e17bbb4c89d29037b19ec18
Iván Trujillo <ivantrujillo@Ivans-MacBook-Pro.local>
2025-03-30 14:24:34 -0500

    clean up
    * clean up

--- jest.config.ui.js
+++ jest.config.ui.js
@@ -1,9 +1,9 @@
 // jest.config.react.js (for client-side tests)
 module.exports = {
-  testEnvironment: "jsdom", // Use jsdom for React components
-  setupFilesAfterEnv: ["@testing-library/jest-dom"], //  Set up @testing-library/jest-dom
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["@testing-library/jest-dom"],
   moduleNameMapper: {
-    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js", //mock css imports
+    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
   },
-  testMatch: ["<rootDir>/src/**/*.test.tsx"], // Only test files in src directory
+  testMatch: ["<rootDir>/src/**/*.test.tsx"],
 };
